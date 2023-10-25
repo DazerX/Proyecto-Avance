c1a7e9b7714231f29f5a4473f6b22ad0d66c58b4
Jose Vidal <142942101+DazerX@users.noreply.github.com>
2023-10-24 21:34:58 -0500

    Add files via upload

---
+++ PROYECTOJADJV(2)/src/Entidades/Vehiculo.java
@@ -0,0 +1,80 @@
+package Entidades;
+
+public class Vehiculo {
+    
+    private String placa;
+    private String marca;
+    private String modelo;
+    private int noCilindro;
+    private int tamRing;
+    private String tVehiculo;
+
+    public Vehiculo(String tVehiculo, String placa, String marca, String modelo, int noCilindro, int tamRing ) {
+        this.tVehiculo=tVehiculo;
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.noCilindro = noCilindro;
+        this.tamRing=tamRing;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getNoCilindro() {
+        return noCilindro;
+    }
+
+    public void setNoCilindro(int noCilindro) {
+        this.noCilindro = noCilindro;
+    }
+
+    public int getTamRing() {
+        return tamRing;
+    }
+
+    public void setTamRing(int tamRing) {
+        this.tamRing = tamRing;
+    }
+
+    public String gettVehiculo() {
+        return tVehiculo;
+    }
+
+    public void settVehiculo(String tVehiculo) {
+        this.tVehiculo = tVehiculo;
+    }
+
+    @Override
+    public String toString() {
+        return  "Placa=" + placa +"\n"+ 
+                "Marca=" + marca +"\n"+ 
+                "Modelo=" + modelo +"\n"+ 
+                "NoCilindro=" + noCilindro +"\n"+ 
+                "Tamaño Ring=" + tamRing +"\n"+ 
+                "Tipo de Vehiculo=" + tVehiculo;
+    }
+    
+    
+}
---
+++ PROYECTOJADJV(2)/src/Vista/Principal.java
@@ -0,0 +1,15 @@
+package Vista;
+
+
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Vista menu = new Vista();
+
+        menu.IniciarSesion();
+
+        //contraseña 1234
+    }
+
+}
